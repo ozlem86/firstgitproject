766bf080ab638effc6c060f8e3fced23865221a6
Ozlem Sert <ozlemksert@gmail.com>
2023-03-12 12:44:05 +0100

    updating my second class

--- src/main/java/first_git_package/MySecondClass.java
+++ src/main/java/first_git_package/MySecondClass.java
@@ -1,8 +1,11 @@
 package first_git_package;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
 public class MySecondClass {
     public static void main(String[] args) {
-        System.out.println("this is my second class");
+        System.out.println("this is my second class,");
+        System.out.println("uppdating existing code");
 
     }
 }
